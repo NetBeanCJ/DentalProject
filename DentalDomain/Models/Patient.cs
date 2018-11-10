85a0560a972969f3addbc4407fb271e298770d62
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-10 17:31:34 +0100

    added prop in patient class

--- DentalDomain/Models/Patient.cs
+++ DentalDomain/Models/Patient.cs
@@ -4,17 +4,21 @@ using System.Text;
 
 namespace DentalProjectDomain.Models
 {
-   public class Patient
+    public class Patient
     {
         public int PatientId { get; set; }
-        public string  SSNR { get; set; }
-        public string  FirstName{ get; set; }
+        public string SSNR { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Street { get; set; }
         public int StrieetNr { get; set; }
         public string City { get; set; }
-        public int PostCode { get; set;} 
-       public int JournalId { get; set; }
+        public int PostCode { get; set; }
+        public int JournalId { get; set; }
+
+        public Journal Journal { get; set; }
+        
+      
     }
 }
