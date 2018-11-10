953635795243ae9e679b87b38cc6ded3c03acf6c
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-10 18:17:11 +0100

    Revert "Revert "Revert "test"""
    This reverts commit 40388617f40fcd8c4b9e02aa8126fabf370b3727.

--- DentalDomain/Models/Phone.cs
+++ DentalDomain/Models/Phone.cs
@@ -2,25 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-using static System.Net.Mime.MediaTypeNames;
-
 namespace DentalProjectDomain.Models
 {
-    
    public class Phone
     {
         public int PhoneId { get; set; }
+      
         public int PatientId { get; set; }
-
-        public string HomeNr { get; set; }
-        public string MobileNr { get; set; }
-
-        public Employee Employee { get; set; }
-
-
 
     }
 }
-
-
- 
