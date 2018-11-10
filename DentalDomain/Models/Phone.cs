7b5b4e3d71b01eb8e7d58b6409120efdf37fde29
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-10 17:31:54 +0100

    added prop in phone class

--- DentalDomain/Models/Phone.cs
+++ DentalDomain/Models/Phone.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DentalProjectDomain.Models
 {
-   public class Phone
+   public class Phone 
     {
         public int PhoneId { get; set; }
         public int PatientId { get; set; }
-
+        public string HomeNr { get; set; }
+        public string MobileNr { get; set; }
+        
+        public Employee Employee { get; set; }
+       
+        
+    
     }
 }
