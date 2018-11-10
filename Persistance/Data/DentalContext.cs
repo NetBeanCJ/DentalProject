3027389c402613887c12570d2d5f89f1eb41098b
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-10 17:32:56 +0100

    added model builder

--- Persistance/Data/DentalContext.cs
+++ Persistance/Data/DentalContext.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using DentalProjectDomain.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.SqlServer;
 
 namespace Persistance.Data
 {
     public class DentalContext : DbContext
     {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+           
+        {
+            OptionsBuilder.UseSqlServer("Data Source = LAPTOP - B9AASP37\\SQLEXPRESS; Initial Catalog = DentalProject; Integrated Security = True; Connect Timeout = 30; ");
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Phone>()
+                .HasOne(ph => ph.Employee)
+                .WithMany(e => e.Phones)
+                .HasForeignKey(ph => ph.PhoneId);
+
+            modelBuilder.Entity<Journal>()
+                .HasOne(j => j.Patients)
+                .WithOne(p => p.Journal)
+                .HasForeignKey<Patient>(p => p.PatientId);
+        }
+       
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        
+        public DbSet<Patient> GetSet { get; set; }
+        public DbSet<Journal> Journals { get; set; }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //private const string ConnectionString = @"server=(localdb)\MSSQLLocalDb;" +
         //   "Database=MenuCards;Trusted_Connection=True";
         //public DbSet<Employee> Employees { get; set; }
@@ -27,7 +70,7 @@ namespace Persistance.Data
         //    modelBuilder.Entity<User>()
         //    .ToTable("Users")
         //    .HasKey(U)
+     
 
-
-        //}
     }
+}
