02631613ebf8b122bd591cb9d1c3a174cd827af3
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-12 16:19:42 +0100

    added person prop

--- DentalDomain/Models/Phone.cs
+++ DentalDomain/Models/Phone.cs
@@ -8,13 +8,12 @@ namespace DentalProjectDomain.Models
    public class Phone 
     {
         public int PhoneId { get; set; }
-        public int PatientId { get; set; }
+        public int PersonId { get; set; }
         public string HomeNr { get; set; }
         public string MobileNr { get; set; }
         
-        public Employee Employee { get; set; }
-       
         
-    
+        public Person Person { get; set; }
+
     }
 }
